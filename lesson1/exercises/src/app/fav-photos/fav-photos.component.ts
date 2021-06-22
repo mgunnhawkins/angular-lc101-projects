a8fee9fa6bcd3c7112528c4b5f485ef5e64b3f9a
Melanie Gunn <melanie@melaniegunn.com>
2021-06-22 14:54:39 -0500

    photos added and font color and style changed

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'My Favorite Photos';
+  image1 = 'https://lh3.googleusercontent.com/Mfoqy0sbdcMGs-Etzl5VQgWLQyjEgei6WSuaYwm7UR1BLJ1LPIcvf3KMM-3sSbsH9QOa33f3kBOFA0c-VBvOcJk6Sap8SFC1mgsfombLF1_Mej_GLUDYnb44duOikbeLi1r0X3nzlhY=w2400';
+  image2 = "https://lh3.googleusercontent.com/tsWZYaTP0fn_HMMrmIbxfUYPsVSIx53GFckdTw9wW1xcVIHsXTujfYOtScITp0oRYFA-V73WQVimoaJ2EczmIHui_bhURh6v3zshxrBOGxPC3gXefNr4vFz05fLpr9QTti_gE0fx3UM=w2400";
+  image3 = 'https://lh3.googleusercontent.com/uHgek8CuRNZAmaMNJ-wqFYQe4BhwHLbY17ggj0mWcnQHqirZ5VDvb9KVQxtXzVZRfTfpD29UUQSvnnhR9xzC_r7nD-KgyfpwyWdgywSod7-yEmvGIGLCAxA-KBoYGWy8Nod3M3HA-_E=w2400';
 
   constructor() { }
 
