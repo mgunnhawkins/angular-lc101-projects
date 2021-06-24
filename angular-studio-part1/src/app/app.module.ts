04561503729014b3e918e04e2fd7703f803941c7
Melanie Gunn <melanie@melaniegunn.com>
2021-06-23 19:49:37 -0500

    background image added

--- angular-studio-part1/src/app/app.module.ts
+++ angular-studio-part1/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './header/header.component';
 import { CrewComponent } from './crew/crew.component';
 import { EquipmentComponent } from './equipment/equipment.component';
 import { ExperimentsComponent } from './experiments/experiments.component';
+import { BackgroundComponent } from './background/background.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { ExperimentsComponent } from './experiments/experiments.component';
     HeaderComponent,
     CrewComponent,
     EquipmentComponent,
-    ExperimentsComponent
+    ExperimentsComponent,
+    BackgroundComponent
   ],
   imports: [
     BrowserModule
